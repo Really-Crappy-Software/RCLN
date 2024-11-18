f1c69dc2c3a5ca00a7fbb004fa2dea5f48f87837
Rioboyva2554 <116155137+Rioboyva2554@users.noreply.github.com>
2024-11-18 17:44:20 +1100

    added file checking

--- rcln.c
+++ rcln.c
@@ -3,12 +3,23 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+  FILE *test;
   if (strcmp(argv[1], "-h") == 0) {
+    test = fopen(argv[2], "r");
+    if (test == NULL) {
+      printf("rcln: %s Does not exist, please try again. \n", argv[2]);
+      return -1;
+    }
     link(argv[2], argv[3]);
   } else if (strcmp(argv[1], "-s") == 0) {
+      test = fopen(argv[2], "r");
+    if (test == NULL) {
+      printf("rcln: %s Does not exist, please try again. \n", argv[2]);
+      return -1;
+    }
     symlink(argv[2], argv[3]);
   } else if (strcmp(argv[1], "-help") == 0) {
     printf("SYNTAX: rcln Original Link \n -h: creates a hard link \n -s: creates a symbolic link \n -help: displays this menu \n If you have any issues report them on Github. \n");
-  } else 
+  } else
   printf("SYNTAX: rcln Orignal Link \n -h: creates a hard link \n -s: creates a symbolic link \n -help: displays this menu \n If you have any issues report them on Github. \n");
 }
